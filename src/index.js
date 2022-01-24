99c3428bcb61058200a64c8cfe78545a01e461bb
rafaelarissa <rafaela.larissalj@gmail.com>
2022-01-23 22:29:40 -0300

    criado requisiçao get para rota tweets

--- src/index.js
+++ src/index.js
@@ -17,5 +17,12 @@ server.post('/tweets', (req, res) => {
     const tweet = req.body;
     tweets.push(tweet);
     res.send(tweet);
-})
+});
+
+server.get('/tweets', (req, res) => {
+    for (let i = 0; i < 10; i++) {
+        res.send(tweets[i]);
+    }
+});
+
 server.listen(5000);
