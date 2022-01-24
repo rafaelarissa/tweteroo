67259fb77cee2fb82e55fd051aa5b69f1af82ac4
rafaelarissa <rafaela.larissalj@gmail.com>
2022-01-23 22:15:53 -0300

    criado requisiçao post para rota tweets

--- src/index.js
+++ src/index.js
@@ -13,4 +13,9 @@ server.post('/sign-up', (req, res) => {
     res.send(usuario);
 });
 
+server.post('/tweets', (req, res) => {
+    const tweet = req.body;
+    tweets.push(tweet);
+    res.send(tweet);
+})
 server.listen(5000);
