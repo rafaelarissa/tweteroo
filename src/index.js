170089248d8eeba849871a165504146351795c15
rafaelarissa <rafaela.larissalj@gmail.com>
2022-01-23 22:59:04 -0300

    requisiçoes post enviando mensagem 'ok'

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 
 const server = express();
 server.use(express.json());
+server.use(cors());
 
 const usuarios = [];
 const tweets = [];
@@ -10,18 +11,18 @@ const tweets = [];
 server.post('/sign-up', (req, res) => {
     const usuario = req.body;
     usuarios.push(usuario);
-    res.send(usuario);
+    res.send('OK');
 });
 
 server.post('/tweets', (req, res) => {
     const tweet = req.body;
     tweets.push(tweet);
-    res.send(tweet);
+    res.send('OK');
 });
 
 server.get('/tweets', (req, res) => {
     for (let i = 0; i < 10; i++) {
-        res.send(tweets[i]);
+        res.send(tweets);
     }
 });
 
